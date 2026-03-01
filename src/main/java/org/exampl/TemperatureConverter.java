2d0826ad25dbc06eb812f545550848f7991e3034
Jiya <jiya.kanhirathanpoyil@metropolia.fi>
2026-03-01 23:47:06 +0200

    Added dockerfile

--- src/main/java/org/exampl/TemperatureConverter.java
+++ src/main/java/org/exampl/TemperatureConverter.java
@@ -1,6 +1,5 @@
 package org.exampl;
 
-
 public class TemperatureConverter {
 
     // Convert Fahrenheit to Celsius
@@ -13,13 +12,22 @@ public class TemperatureConverter {
         return (celsius * 9 / 5) + 32;
     }
 
-    // Convert KelvinS to Celsius
+    // Convert Kelvin to Celsius
     public double kelvinToCelsius(double kelvin) {
         return kelvin - 273.15;
     }
 
-    // Check if temperature is extreme (in Celsius)
+    // Check if temperature is extreme
     public boolean isExtremeTemperature(double celsius) {
         return celsius < -40 || celsius > 50;
+    }
+
+    // MAIN METHOD (needed for Docker)
+    public static void main(String[] args) {
+        TemperatureConverter converter = new TemperatureConverter();
+
+        double result = converter.fahrenheitToCelsius(100);
+
+        System.out.println("100°F in Celsius = " + result);
     }
 }
